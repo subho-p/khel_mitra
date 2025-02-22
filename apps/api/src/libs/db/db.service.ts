9e539511adc5ca5b08e8ed7ef403909940404ec1
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-02-22 11:58:49 +0530

    feat: init socket

--- apps/api/src/libs/db/db.service.ts
+++ apps/api/src/libs/db/db.service.ts
@@ -4,10 +4,13 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     async onModuleInit() {
-        await this.$connect();
+        if (process.env.NODE_ENV === 'development') {
+            await this.$connect();
+        }
     }
 
     async onModuleDestroy() {
         // Perform any necessary database migrations or seeding here
+        await this.$disconnect();
     }
 }
