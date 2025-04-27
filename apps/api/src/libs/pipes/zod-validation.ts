221d52a421ecea57c0118e5f71a5faa8aecab030
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-04-27 20:19:44 +0530

    feat(validation): implement ZodValidationPipe and add zodValidation utility function

--- apps/api/src/libs/pipes/zod-validation.ts
+++ apps/api/src/libs/pipes/zod-validation.ts
@@ -2,20 +2,27 @@ import { BadRequestException, PipeTransform, Logger, ArgumentMetadata } from '@n
 import * as z from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
-    constructor(private schema: z.ZodSchema) {}
+    private logger: Logger;
+    constructor(private schema: z.ZodSchema) {
+        this.logger = new Logger('Zod Validation');
+    }
 
     transform(value: unknown, meatdata: ArgumentMetadata) {
         try {
-            const parsedValue = this.schema.parse(value);
-            return parsedValue;
+            this.logger.debug(value);
+            const parsedValue = this.schema.safeParse(value);
+            if (!parsedValue.success) {
+                throw parsedValue.error;
+            }
+            return parsedValue.data;
         } catch (error: unknown) {
             if (error instanceof z.ZodError) {
                 const message = error.errors[0].message;
-                Logger.error(message, meatdata.type);
+                this.logger.error(message, meatdata.type);
                 throw new BadRequestException(message || 'Validation failed');
             }
 
-            Logger.error(String(error), meatdata.type);
+            this.logger.error(String(error), meatdata.type);
             throw new BadRequestException('Validation failed');
         }
     }
